3a1e77a1c12127ebe5b85219770c328eb8ec9d95
KoenigJCS <jkoenig@nevada.unr.edu>
2025-05-06 20:01:10 -0700

    Small Change

--- MinesweeperGUI.py
+++ MinesweeperGUI.py
@@ -113,7 +113,7 @@ class MinesweeperGUI:
                 elif tile.isRevealed:
                     btn.config(text=" ", state="disabled", relief="sunken", bg="lightgrey")
                 elif tile.isFlagCovered:
-                    btn.config(text="F", fg="red", bg="#D9E2F3")
+                    btn.config(text="F", fg="red", bg="#FFFFFF")
                 else:
                     btn.config(text="", fg="black", bg="#E1EFFF")
 
